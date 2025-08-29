a21b195250cd866061c6e634e6845521d748bc21
TodMount <todmount@gmail.com>
2025-08-29 11:54:32 +0300

    feat: Expand Merriam-Webster audio search logic

--- sources/mw_dict_api.py
+++ sources/mw_dict_api.py
@@ -14,7 +14,7 @@ class FetchMWDictAPI(GetAudio):
         super().__init__(output_dir, name="Merriam-Webster API")
         self.country_codes = ["uk", "us"]
 
-    def built_url(self, word: str, api_key: str):
+    def built_url(self, word: str, api_key: str) -> str:
         if api_key is None:
             raise ValueError("No API key provided")
         return f"https://www.dictionaryapi.com/api/v3/references/learners/json/{word}?key={api_key}"
@@ -22,9 +22,27 @@ class FetchMWDictAPI(GetAudio):
     def parse_response(self, response):
         return response.json()
 
+    def find_audio(self, data) -> str:
+        """Additional search logic for Merriam-Webster. For now, it just takes all audio in a list and returns the first element"""
+        audio_files = []
+
+        for entry in data:
+            # 1) Normal case: pronunciation in hwi.prs
+            if "hwi" in entry:
+                for prs in entry["hwi"].get("prs", []):
+                    if "sound" in prs:
+                        audio_files.append(prs["sound"]["audio"])
+            # 2) Variant case: pronunciation in vrs[i].prs
+            for vr in entry.get("vrs", []):
+                for prs in vr.get("prs", []):
+                    if "sound" in prs:
+                        audio_files.append(prs["sound"]["audio"])
+
+        return audio_files[0]
+
     def extract_candidate(self, data):
         try:
-            audio_filename = data[0].get("hwi", {}).get("prs", [])[0].get("sound", {}).get("audio")
+            audio_filename = self.find_audio(data)
             if not audio_filename:
                 raise AudioNotFound
             if audio_filename[0].isdigit() or not audio_filename[0].isalpha():
