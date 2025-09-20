a9dc03c52d5be1eb4db19ea8e5bae70ff819d543
TodMount <todmount@gmail.com>
2025-09-20 13:05:28 +0300

    refactor: Make 'setup_logger.py' print startup info only for main script

--- common/setup_logger.py
+++ common/setup_logger.py
@@ -6,28 +6,31 @@ from pathlib import Path
 
 
 CONSOLE_FORMATTER = logging.Formatter("%(message)s")
-FILE_FORMATTER = logging.Formatter('%(asctime)s | %(levelname)-8s | %(name)-8s | %(message)s')
+FILE_FORMATTER = logging.Formatter('%(asctime)s | %(levelname)-8s | %(name)-22s | %(message)s')
 
 
 def setup_logger(
         name: str,
-        log_name_dir: Path,
+        log_file_dir: Path,
         log_file_name: str = 'main.log',
         max_bytes: int = 5*1024*1024,
-        backup_count: int = 3
+        backup_count: int = 3,
+        is_main: bool = False
 ):
-    """Setup logger with rich console and file handlers."""
-    if log_file_name.split('.')[-1]:
-        raise ValueError("Incorrect log file extension, please check your 'setup_logger'")
+    """Setup logger with rich console and file handlers"""
+    log_file_name = Path(log_file_name)
+    log_ext = log_file_name.suffix
+    if log_ext != '.log':
+        raise ValueError(f"Incorrect log file extension, please check your 'setup_logger'. Your current is {log_ext}")
     # Get the main logger
     logger = logging.getLogger(name)
-    logger.setLevel(logging.DEBUG)  # Set the logger level to INFO
+    logger.setLevel(logging.DEBUG)
 
 
     if not logger.handlers:
         console_handler = RichHandler(
             show_time=False,
-            show_level=True,
+            show_level=False,
             show_path=False,
             rich_tracebacks=True
         )
@@ -35,8 +38,8 @@ def setup_logger(
         console_handler.setFormatter(CONSOLE_FORMATTER)
         logger.addHandler(console_handler)
 
-        log_name_dir.mkdir(parents=True, exist_ok=True)
-        log_file_path = log_name_dir / log_file_name
+        log_file_dir.mkdir(parents=True, exist_ok=True)
+        log_file_path = log_file_dir / log_file_name
 
         file_handler = RotatingFileHandler(
             filename=log_file_path,
@@ -47,11 +50,12 @@ def setup_logger(
         file_handler.setFormatter(FILE_FORMATTER)
         logger.addHandler(file_handler)
 
-    # Add startup marker
-    logger.debug("=" * 50)
-    logger.debug("NEW APPLICATION INSTANCE STARTED")
-    logger.debug(f"PID: {os.getpid()} | Python: {sys.version.split()[0]}")
-    logger.debug("=" * 50)
+    if is_main:
+        # Add startup marker
+        logger.debug("=" * 50)
+        logger.debug("NEW APPLICATION INSTANCE STARTED")
+        logger.debug(f"PID: {os.getpid()} | Python: {sys.version.split()[0]}")
+        logger.debug("=" * 50)
 
     return logger
 
